f3c28f7306f46e6d797c9a99201be65e80efed5d
Aymeric Pellé <aympelle@gmail.com>
2024-12-18 08:44:28 +0100

    v0.1.0:#1: Extract policy features from arba-core.
    ThreadPolicy
    ExceptionPolicy

---
+++ test/thread_policy_tests.cpp
@@ -0,0 +1,18 @@
+#include <arba/meta/policy/thread_policy.hpp>
+#include <gtest/gtest.h>
+
+int func(meta::ThreadPolicy auto)
+{
+    return 0;
+}
+
+int func(meta::thread_safe_t)
+{
+    return 642;
+}
+
+TEST(thread_policy_tests, test_thread_policy)
+{
+    ASSERT_EQ(func(meta::thread_safe), 642);
+    ASSERT_EQ(func(meta::thread_unsafe), 0);
+}
