f144c0da0cb49a6526ded23dde5b4ce94f12c67c
Aymeric Pellé <aympelle@gmail.com>
2024-12-19 23:54:22 +0100

    v0.1.0:#1: Extract is_moveable_to_itself from arba-core.

---
+++ include/arba/meta/type_traits/is_moveable_to_itself.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+#include <type_traits>
+
+inline namespace arba
+{
+namespace meta
+{
+
+template <class Type>
+inline constexpr bool is_move_assignable_to_itself_v = std::is_trivially_copyable_v<Type>;
+
+template <class Type>
+inline constexpr bool is_move_assignable_to_itself_v<std::unique_ptr<Type>> = true;
+
+template <class Type>
+inline constexpr bool is_move_assignable_to_itself_v<std::shared_ptr<Type>> = true;
+
+template <class Type>
+    requires std::is_scalar_v<Type>
+inline constexpr bool is_move_assignable_to_itself_v<std::atomic<Type>> = true;
+
+template <class Type>
+inline constexpr bool is_move_assignable_to_itself_v<std::atomic<std::shared_ptr<Type>>> = true;
+
+}
+}
