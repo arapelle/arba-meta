ce0039207dc0d307015ddfa93e868b4bcf95ccce
Aymeric Pellé <aympelle@gmail.com>
2024-12-19 01:11:49 +0100

    v0.1.0:#1: Extract default_t/v, parameter_type_or_t from arba-core. Add value_or functions.
    Rename parameter_type_or_t type_ot_t.
    Add value_or().
    Add value_or_generate().
    Add remove_right_reference_t.

---
+++ test/default_arg.hpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+class default_arg
+{
+public:
+    static unsigned& destructor_counter()
+    {
+        static unsigned counter = 0;
+        return counter;
+    }
+
+    int value;
+
+    operator int() const { return value; }
+
+    explicit default_arg() : value(1000) {}
+
+    default_arg(default_arg&& other) : value(other.value) { other.value = 0; }
+
+    default_arg(const default_arg& other) = delete;
+
+    ~default_arg()
+    {
+        if (value)
+            ++destructor_counter();
+    }
+
+    default_arg& operator=(default_arg&& other)
+    {
+        if (this != &other)
+        {
+            value = other.value;
+            other.value = 0;
+        }
+        return *this;
+    }
+
+    default_arg& operator=(const default_arg& other) = delete;
+};
