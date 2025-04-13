94183d81f490eab5edc27df1378f03fabdc4d951
Aymeric Pellé <aympelle@gmail.com>
2025-04-13 18:59:42 +0200

    v0.4.0:#14: Adds concept EndiannessPolicy.
    This commit introduces an endianness policy to handle
    endianness-specific and endianness-neutral scenarios.
    
    It defines `endianness_neutral_t` and `endianness_specific_t`
    types along with a concept `EndiannessPolicy` to validate
    the policy type.

---
+++ include/arba/meta/policy/endianness_policy.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <type_traits>
+
+inline namespace arba
+{
+namespace meta
+{
+
+struct endianness_neutral_t
+{
+    explicit endianness_neutral_t() = default;
+};
+constexpr endianness_neutral_t endianness_neutral;
+
+struct endianness_specific_t
+{
+    explicit endianness_specific_t() = default;
+};
+constexpr endianness_specific_t endianness_specific;
+
+template <class PolicyType>
+concept EndiannessPolicy = std::is_same_v<PolicyType, endianness_neutral_t> || std::is_same_v<PolicyType, endianness_specific_t>;
+
+} // namespace meta
+} // namespace arba
