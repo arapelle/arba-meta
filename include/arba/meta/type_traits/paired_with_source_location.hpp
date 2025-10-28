6bdf3d4dc5d4d981ec7c5b20989d3c3d4651a736
Aymeric Pellé <aympelle@gmail.com>
2025-10-28 07:04:26 +0100

    v0.7.0:#30: Renames type_and_source_location paired_with_source_location.

--- include/arba/meta/type_traits/paired_with_source_location.hpp
+++ include/arba/meta/type_traits/paired_with_source_location.hpp
@@ -10,28 +10,28 @@ namespace meta
 {
 
 template <class Type>
-struct type_and_source_location : public std::pair<Type, std::source_location>
+struct paired_with_source_location : public std::pair<Type, std::source_location>
 {
-    type_and_source_location(const Type& arg, const std::source_location loc = std::source_location::current())
+    paired_with_source_location(const Type& arg, const std::source_location loc = std::source_location::current())
         : std::pair<Type, std::source_location>(arg, loc)
     {
     }
 
-    type_and_source_location(Type&& arg, const std::source_location loc = std::source_location::current())
+    paired_with_source_location(Type&& arg, const std::source_location loc = std::source_location::current())
         : std::pair<Type, std::source_location>(std::forward<Type>(arg), loc)
     {
     }
 
     template <class Tp>
         requires(std::convertible_to<Tp, Type>)
-    type_and_source_location(const Tp& arg, const std::source_location loc = std::source_location::current())
+    paired_with_source_location(const Tp& arg, const std::source_location loc = std::source_location::current())
         : std::pair<Type, std::source_location>(Type(arg), loc)
     {
     }
 
     template <class Tp>
         requires(std::convertible_to<Tp, Type>)
-    type_and_source_location(Tp&& arg, const std::source_location loc = std::source_location::current())
+    paired_with_source_location(Tp&& arg, const std::source_location loc = std::source_location::current())
         : std::pair<Type, std::source_location>(Type(std::forward<Tp>(arg)), loc)
     {
     }
